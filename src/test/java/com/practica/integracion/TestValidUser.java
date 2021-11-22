0bf983d64673fad8e31de4a30e2e7f9b317b8b69
gonzalomdvc <gonzalo.mdvc@gmail.com>
2021-11-22 12:39:01 +0100

    TestValidUser empezado

--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -1,13 +1,19 @@
 package com.practica.integracion;
 
+import com.practica.integracion.DAO.AuthDAO;
+import com.practica.integracion.DAO.User;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class TestValidUser {
 
-	/**
-	 * RELLENAR POR EL ALUMNO
-	 */
+	@Mock
+	private User user;
+
+	@Spy
+	private AuthDAO auth;
 
 }
