09b83ceca51f9688369691566b5b8dabea024e0b
ivicario <i.vicario@alumnos.upm.es>
2021-11-29 12:34:03 +0100

    test 3

--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -82,9 +82,8 @@ public class TestValidUser {
 		User validUser = new User("1","Ana","Lopez","Madrid", (List<Object>) new ArrayList<Object>(Arrays.asList(1, 2)));
 		  when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
 		  
-		  String validId = "12345"; // id valido de sistema
-		  ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
-		  when(mockGenericDao.getSomeData(validUser, "where id=" + validId)).thenReturn(lista);
+		  Object objetoValido = "12345";
+		  when(mockGenericDao.updateSomeData(validUser, objetoValido)).thenReturn(true);
 		  
 		// primero debe ejecutarse la llamada al dao de autenticación
 		  // despues el de  acceso a datos del sistema (la validaciones del orden en cada prueba)
@@ -93,11 +92,11 @@ public class TestValidUser {
 		// instanciamos el manager con los mock creados
 		  SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
 		  
-		  manager.addRemoteSystem(validUser.getId(), validId);
+		  manager.addRemoteSystem(validUser.getId(), objetoValido);
 		  
 		  // vemos si se ejecutan las llamadas a los dao, y en el orden correcto
 		  ordered.verify(mockAuthDao).getAuthData(validUser.getId());
-		  ordered.verify(mockGenericDao).updateSomeData(validUser, "where id=" + validId);
+		  ordered.verify(mockGenericDao).updateSomeData(validUser, objetoValido);
 		  
 		
 	}
