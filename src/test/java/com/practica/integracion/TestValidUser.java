eb9aeae675123f5bea30be6f7c25fc6d69ae4153
robertomancebom <roberto.mancebom@alumnos.upm.es>
2021-11-29 12:10:19 +0100

    add test stop remote valid system

--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -32,7 +32,7 @@ public class TestValidUser {
 
 	@Test
 	public void testStartRemoteSystemValidSystem() throws Exception{
-		User validUser = new User("1","Ana","Lopez","Madrid", (List<Object>) new ArrayList<Object>(Arrays.asList(1, 2)));
+		  User validUser = new User("1","Ana","Lopez","Madrid", (List<Object>) new ArrayList<Object>(Arrays.asList(1, 2)));
 		  when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
 
 		  String validId = "12345"; // id valido de sistema
@@ -53,7 +53,28 @@ public class TestValidUser {
 	
 	@Test
 	public void testStopRemoteSystemValidSystem()  throws Exception{
+		  
+		  User validUser = new User("1","Ana","Lopez","Madrid", (List<Object>) new ArrayList<Object>(Arrays.asList(1, 2)));
+		  when(mockAuthDao.getAuthData(validUser.getId())).thenReturn(validUser);
 		
+		  String validId = "12345"; // id valido de sistema
+		  ArrayList<Object> lista = new ArrayList<>(Arrays.asList("uno", "dos"));
+		  when(mockGenericDao.getSomeData(validUser, "where id=" + validId)).thenReturn(lista);
+		  
+		  // primero debe ejecutarse la llamada al dao de autenticación
+		  // despues el de  acceso a datos del sistema (la validaciones del orden en cada prueba)
+		  InOrder ordered = inOrder(mockAuthDao, mockGenericDao);
+		  
+		  // instanciamos el manager con los mock creados
+		  SystemManager manager = new SystemManager(mockAuthDao, mockGenericDao);
+		  
+		  // llamada al api a probar
+		  Collection<Object> retorno = manager.stopRemoteSystem(validUser.getId(), validId);
+		  assertEquals(retorno.toString(), "[uno, dos]");
+		  
+		  // vemos si se ejecutan las llamadas a los dao, y en el orden correcto
+		  ordered.verify(mockAuthDao).getAuthData(validUser.getId());
+		  ordered.verify(mockGenericDao).getSomeData(validUser, "where id=" + validId);
 	}
 	@Test
 	public void testAddRemoteSystemValidSystem()  throws Exception{
